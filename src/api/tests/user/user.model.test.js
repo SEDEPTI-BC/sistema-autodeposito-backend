7ff77c9e825f7f1402089e47658dd7cb4906a377
Samantha Silva <samanthaathayde@hotmail.com>
2021-12-30 16:08:07 -0300

    feat: test user repository

---
+++ src/api/tests/user/user.model.test.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+const connection = require('../../../db/connection');
+const db = require('../../../db/database');
+const user = require('../../models/user');
+
+describe('user model', () => {
+  beforeAll(async () => {
+    await connection;
+  });
+
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  afterAll(async () => {
+    await db.close();
+  });
+
+  it('should create user', async () => {
+    const User = user(db, DataTypes);
+    const newUser = await User.create({
+      username: 'john',
+      email: 'john@email.com',
+      password: 'john123',
+      role: 'admin',
+    });
+
+    expect(newUser.username).toBe('john');
+  });
+});
